f1b482304b0422433a15196516c8b2d6a486669e
Gustavo Oliv <calacogustavo@gmail.com>
2026-01-17 18:01:55 -0300

    salvar nome do usuário somente no state em memória

--- context/authContext.js
+++ context/authContext.js
@@ -1,39 +1,18 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useContext, useState } from "react";
 import api from "../app/services/api";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        async function loadUser() {
-            const storedUser = await AsyncStorage.getItem("@user");
-            const token = await AsyncStorage.getItem("@token");
-
-            if (storedUser && token) {
-                api.defaults.headers.common.Authorization = `Bearer ${token}`;
-                setUser(JSON.parse(storedUser));
-            }
-
-            setLoading(false);
-        }
-
-        loadUser();
-    }, []);
 
     async function login({ token, user }) {
-        await AsyncStorage.setItem("@token", token);
-        await AsyncStorage.setItem("@user", JSON.stringify(user));
-
         api.defaults.headers.common.Authorization = `Bearer ${token}`;
         setUser(user);
     }
 
     async function logout() {
-        await AsyncStorage.multiRemove(["@token", "@user"]);
+        api.defaults.headers.common.Authorization = null;
         setUser(null);
     }
 
@@ -43,15 +22,7 @@ export function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider
-            value={{
-                user,
-                loading,
-                login,
-                logout,
-                deleteAccount,
-            }}
-        >
+        <AuthContext.Provider value={{ user, login, logout, deleteAccount}}>
             {children}
         </AuthContext.Provider>
     );
