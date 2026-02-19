83b77bf4f4f1a31a5e90fa642b09a73a7ed4863d
Gustavo Oliv <calacogustavo@gmail.com>
2026-02-19 17:39:04 -0300

    refactor: pastas dentro de /src

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function (api) {
+export default function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            'react-native-reanimated/plugin', // <- este precisa ser o último plugin
-        ],
+            'react-native-reanimated/plugin',
+        ]
     };
 };
